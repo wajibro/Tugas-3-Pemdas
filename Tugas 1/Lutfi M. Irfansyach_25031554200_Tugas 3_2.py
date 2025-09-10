80f7a70716b97e0838d8226008a6a53134057a85
upi <wajibro@yahoo.com>
2025-09-11 05:10:36 +0700

    fix bug

--- Tugas 1/Lutfi M. Irfansyach_25031554200_Tugas 3_2.py
+++ Tugas 1/Lutfi M. Irfansyach_25031554200_Tugas 3_2.py
@@ -1,24 +1,21 @@
 x = [3,4,5,2,1,10,8,77,60,50]
-i = 0
+j = 0
 maks = 0
 
 # for Loop
 for i in range( len(x)):
   if x[i] > maks:
     maks = x[i]
-  elif x[i-1] > maks:
-    maks = x[i-1]
   else:
     continue
+
 print(maks)
 maks = 0
 
 # while Loop
-while i < len(x):
-  if x[i] > maks:
-    maks = x[i]
-  elif x[i-1] > maks:
-    maks = x[i-1]
-  else:
-    continue
-print("cihuy")
+while j < len(x):
+  if x[j] > maks:
+    maks = x[j]
+  j += 1
+
+print(maks)
