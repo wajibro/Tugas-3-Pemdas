108cbfb66bf98d4d1896d1924676bec058228c1b
upi <wajibro@yahoo.com>
2025-09-11 20:57:38 +0700

    Menambahkan flowchart

--- Tugas 1/Lutfi M. Irfansyach_25031554200_Tugas 3_2.py
+++ Tugas 1/Lutfi M. Irfansyach_25031554200_Tugas 3_2.py
@@ -1,6 +1,41 @@
+def flowchart():
+  print("""
+Isi dari array x = 3, 4, 5, 2, 1, 10, 8, 77, 60, 50
+
+Flowchart pencarian data maks menggunakan for & while loop :
+
+                            Atur nilai maks bernilai 0
+                                      |
+                                      |
+                                      |
+                                      V
+                Periksa data array [x] dengan perulangan tiap [i] <-------------------> Semua x sudah di periksa
+                                      |                                |                           |
+                                      |                                |                           |
+                                      |                                |                           |
+                                      |                                |                           |
+                                      V                                |                           |
+                Apakah nilainya lebih besar dari variabel maks?        |                           |
+                                      |                                |                           |
+                                      |                                |                           |
+                                      |                                |                           |
+                                      V                                |                           |                  
+                [Ya]<-------------------------------------->[Tidak]-----                           |
+                  |                                         continue   A                           |
+                  |                                                    |                           |
+                  |                                                    |                           |
+                  V                                                    |                           |
+Simpan nilai tersebut kedalam variabel maks ----------------------------                           |
+                                                                                                   |
+                                                                                                   V
+                                                                              Tampilkan nilai maks akhir dari perulangan
+        """)
+
 x = [3,4,5,2,1,10,8,77,60,50]
 j = 0
 maks = 0
+
+flowchart()
 
 # for Loop
 for i in range( len(x)):
@@ -9,7 +44,7 @@ for i in range( len(x)):
   else:
     continue
 
-print(maks)
+print("Nilai maks menggunakan for =", maks)
 maks = 0
 
 # while Loop
@@ -18,4 +53,4 @@ while j < len(x):
     maks = x[j]
   j += 1
 
-print(maks)
+print("Nilai maks menggunakan while =", maks)
